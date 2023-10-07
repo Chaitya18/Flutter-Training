5865cf7e0a258bbf6f772997c8139721cb48973e
Chaitya18 <chaityashah1819@gmail.com>
2023-10-07 15:34:48 +0530

    Assignments

--- Assignments/Module1/Q3.dart
+++ Assignments/Module1/Q3.dart
@@ -1,1 +1,26 @@
+import 'dart:io';
 
+void main() {
+  int? number;
+  int? choice;
+  print("Enter a number:");
+  number = int.parse(stdin.readLineSync()!);
+  print("Press 1 for Square  Press 2 for Cube");
+  choice = int.parse(stdin.readLineSync()!);
+  switch (choice) {
+    case 1:
+      {
+        print(number * number);
+        break;
+      }
+    case 2:
+      {
+        print(number * number * number);
+        break;
+      }
+    default:
+      {
+        print("Invalid Choice");
+      }
+  }
+}
