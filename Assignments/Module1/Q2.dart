5f302ca59339bc8a2c4fc6e64ad0c40a9cc96646
Chaitya18 <chaityashah1819@gmail.com>
2023-10-07 14:07:08 +0530

    Assignments

---
+++ Assignments/Module1/Q2.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  int num1;
+  int num2;
+  print("Enter Numer 1:");
+  num1 = int.parse(stdin.readLineSync()!);
+  print("Enter Numer 2:");
+  num2 = int.parse(stdin.readLineSync()!);
+  print("num1 + num2");
+  print(num1 + num2);
+  print("Subtraction:");
+  print(num1 - num2);
+  print("Multiplication:");
+  print(num1 * num2);
+  print("Division");
+  print(num1 / num2);
+}
